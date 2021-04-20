f3f57e6c7b6d55125d698d5c5c82847021bb8850
dannyirwin <dirwin123@gmail.com>
2021-04-20 13:52:33 -0600

    Add Woodworking and Dev Sections

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ handleSlideDisplay(currentSlideIndex);
 
 
 function handleNextSlide(n) {
-    console.log("Going to next slide");
-    console.log(currentSlideIndex)
     currentSlideIndex += n;
-    handleSlideDisplay(currentSlideIndex);
+    handleSlideDisplay();
 }
 
 function handleSlideDisplay(n) {
     let slides = document.getElementsByClassName("slide");
     let navBtns = document.getElementsByClassName("nav-btn");
 
-    if (n > slides.length - 1) { currentSlideIndex = 0 };
-    if (n < 0) { currentSlideIndex = slides.length - 1 }
+    if (typeof n == 'number') {
+        currentSlideIndex = n;
+    }
+
+    if (currentSlideIndex > slides.length - 1) { currentSlideIndex = 0 };
+    if (currentSlideIndex < 0) { currentSlideIndex = slides.length - 1 };
 
     for (let slide of slides) {
         slide.style.display = "none";
